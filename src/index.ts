886a3933f87aa1772bc63006c41417bf40cd4aac
julz <owanojulius742002@gmail.com>
2025-03-22 10:35:57 +0800

    Added static html to display all endpoints

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 app.use(cors({ origin: "*" }));
 
-app.use('/', router)
+app.use('/', router);
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
-})
+});
 
 export default app;
